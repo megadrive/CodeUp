bdc13dd5d49bf3ab99eae176e2246762c81a042f
Andy <a.simeon@gmail.com>
2019-03-07 21:45:12 +1100

    initial commit, cli working

---
+++ cli.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const argv = require('yargs').argv;
+const {
+    readFile
+} = require("mz/fs");
+const {
+    join
+} = require("path");
+const pkg = require("./package.json");
+
+const codeup = require("./lib");
+const transportRegistry = require("./lib/transportRegistry");
+
+const errors = [];
+
+;
+(async () => {
+    try {
+        const transports = await transportRegistry.loadTransports();
+        const transportExists = transportRegistry.exists(argv.transport);
+        const file = argv._[0]; // first only
+        const transport = argv.transport ? argv.transport : "hasteb.in";
+
+        if (argv.version) {
+            console.log(pkg.version);
+            process.exit(0);
+        }
+
+        if (argv.list) {
+            console.log("Available transports: " + Object.keys(transports).join(", "));
+            process.exit(0);
+        }
+
+        if (!argv.transport) {
+            if (argv.v) console.warn("No transport defined, using hasteb.in");
+        } else if (!transportExists) {
+            errors.push(`Need a defined transport (got ${argv.transport}): --transport <transportname>`);
+        }
+
+        if (!file) {
+            errors.push("No file specified.");
+        }
+
+        if (errors.length) {
+            errors.forEach(error => console.error("Error: " + error));
+            process.exit(1);
+        }
+
+        const filepath = join(file);
+        const code = await readFile(filepath, "utf8");
+
+        const options = {
+            filename: file,
+            filetype: file.split(".").reverse()[0]
+        };
+
+        const finalisedUrl = await codeup.run(transports[transport], code, options);
+
+        console.log(finalisedUrl);
+    } catch (err) {
+        console.error(err);
+    }
+})();
